7c76c2077ea22a9d7dc45a93ec8ea802296c1a8a
palloncino <antonio.guiotto.dev@gmail.com>
2023-12-18 15:07:59 -0600

    phase 2 raw

--- animations/project-eclipse/main.js
+++ animations/project-eclipse/main.js
@@ -7,50 +7,82 @@ document.addEventListener("DOMContentLoaded", () => {
   canvas.width = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
 
-  let centerX = canvas.width / 2; // Center X position
   let atom1InitialX = canvas.width * 0.1; // Starting X for atom1 aligned with the initial drawing position
   let atom2InitialX = canvas.width * 0.9; // Starting X for atom2 aligned with the initial drawing position
+
+  let centerX = canvas.width / 2; // Center X position
+  let atoms = [
+    { x: atom1InitialX, y: canvas.height / 2, color: 'green' },
+    { x: atom2InitialX, y: canvas.height / 2, color: 'blue' },
+    // Additional atoms for phase 2
+    { x: centerX, y: canvas.height / 2, color: 'red' },
+    { x: centerX, y: canvas.height / 2, color: 'orange' },
+    { x: centerX, y: canvas.height / 2, color: 'yellow' },
+    { x: centerX, y: canvas.height / 2, color: 'purple' },
+  ];
+
   const MAX_SCROLL_POSITION = 1000; // Define the maximum scroll value
-  const maxScrollPhase1 = 300;
+  const maxScrollPhase1 = 333;
+  const maxScrollPhase2 = 666;
+  const maxScrollPhase3 = 999;
+
+  function drawSpecificAtoms(atomsToDraw) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    atomsToDraw.forEach((atom) => drawAtom(atom.x, atom.y, atom.color));
+  }
 
   // Define drawAtom function here to ensure it has access to ctx
-  function drawAtom(x, y) {
+  function drawAtom(x, y, color) {
     ctx.beginPath();
     ctx.arc(x, y, 20, 0, 2 * Math.PI); // Draw a circle for the atom
-    ctx.fillStyle = "blue";
+    ctx.fillStyle = color;
     ctx.fill();
   }
 
   // Initial draw
-  drawAtom(atom1InitialX, canvas.height / 2); // Left atom
-  drawAtom(atom2InitialX, canvas.height / 2); // Right atom
+  drawSpecificAtoms(atoms.slice(0, 2));
 
-  // Define your phase functions here
+  // +-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -+
+  // | ACTION                                             |
+  // +-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -+
   function phase1Animation(scrollPos) {
-    console.log("phase1Animation", { scrollPos });
     let progress = scrollPos / maxScrollPhase1;
-    progress = Math.min(progress, 1); // Cap progress to 1
+    progress = Math.min(progress, 1);
 
-    // Clear the canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // Update positions of the first two atoms
+    atoms[0].x = atom1InitialX + (centerX - atom1InitialX) * progress;
+    atoms[1].x = atom2InitialX - (atom2InitialX - centerX) * progress;
 
-    // Calculate new positions based on scroll progress
-    // Atom 1 moves from left to center
-    let atom1NewX = atom1InitialX + (centerX - atom1InitialX) * progress;
-    // Atom 2 moves from right to center
-    let atom2NewX = atom2InitialX - (atom2InitialX - centerX) * progress;
-
-    // Redraw the atoms at the new positions
-    drawAtom(atom1NewX, canvas.height / 2);
-    drawAtom(atom2NewX, canvas.height / 2);
+    drawSpecificAtoms(atoms.slice(0, 2));
   }
 
   function phase2Animation(scrollPos) {
-    console.log("phase2Animation", { scrollPos });
+    console.log("Phase 2", scrollPos);
+    let progress = (scrollPos - maxScrollPhase1) / (maxScrollPhase2 - maxScrollPhase1);
+    progress = Math.min(progress, 1);
+
+    // Apply an easing function for acceleration and deceleration
+    // Example: Ease out effect
+    let easedProgress = 1 - Math.pow(1 - progress, 2);
+
+    let hexagonVertices = getHexagonVertices(centerX, canvas.height / 2, 250);
+
+    // Start from the center and interpolate towards the hexagon vertices
+    for (let i = 2; i < hexagonVertices.length + 2; i++) {
+      if (atoms[i]) {
+        let targetX = hexagonVertices[i - 2].x;
+        let targetY = hexagonVertices[i - 2].y;
+
+        atoms[i].x = centerX + (targetX - centerX) * easedProgress;
+        atoms[i].y = canvas.height / 2 + (targetY - canvas.height / 2) * easedProgress;
+      }
+    }
+
+    drawSpecificAtoms(atoms);
   }
 
   function phase3Animation(scrollPos) {
-    console.log("phase3Animation", { scrollPos });
+    console.log("Phase 3", scrollPos);
   }
 
   function updateAnimation(scrollPos) {
@@ -83,6 +115,17 @@ document.addEventListener("DOMContentLoaded", () => {
         }, limit - (Date.now() - lastRan));
       }
     };
+  }
+
+  function getHexagonVertices(centerX, centerY, radius, sides = 6) {
+    let vertices = [];
+    for (let i = 0; i < sides; i++) {
+      let angle = (Math.PI / 3) * i; // 60 degrees for hexagon
+      let x = centerX + radius * Math.cos(angle);
+      let y = centerY + radius * Math.sin(angle);
+      vertices.push({ x, y });
+    }
+    return vertices;
   }
 
   // Define throttledUpdate inside the DOMContentLoaded
