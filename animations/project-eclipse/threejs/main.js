689a150842d235671f04b38e539eb403a150725d
palloncino <antonio.guiotto.dev@gmail.com>
2023-12-26 11:34:42 -0500

    formatted

--- animations/project-eclipse/threejs/main.js
+++ animations/project-eclipse/threejs/main.js
@@ -18,23 +18,127 @@ const pointLight = new THREE.PointLight(0xffffff, 1);
 pointLight.position.set(5, 5, 5);
 scene.add(pointLight);
 
+// Interaction
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
+window.addEventListener("mousemove", onMouseMove, false);
 
-// https://threejs.org/examples/fonts/helvetiker_regular.typeface.json
+// Text Loading
 const loader = new THREE.FontLoader();
-const radius = 1.5;
-const orbitRadius = 0.7;
-const textString = "Text"; // Replace with your desired text
-const characters = textString.split("");
-const totalAngle = Math.PI; // Use half a circle (180 degrees) or less for tighter grouping
-const angleStep = totalAngle / characters.length;
+loader.load("https://threejs.org/examples/fonts/helvetiker_regular.typeface.json", addTextToSpheres);
 
-loader.load("https://threejs.org/examples/fonts/helvetiker_regular.typeface.json", function (font) {
+// Sphere Creation
+const sphereGeometry = new THREE.SphereGeometry(0.5, 32, 32);
+const sphereMaterials = [
+  new THREE.MeshStandardMaterial({ color: 0xff0000 }), // red
+  new THREE.MeshStandardMaterial({ color: 0x00ff00 }), // green
+  new THREE.MeshStandardMaterial({ color: 0x0000ff }), // blue
+  new THREE.MeshStandardMaterial({ color: 0xffff00 }), // yellow
+  new THREE.MeshStandardMaterial({ color: 0x00ffff }), // aqua
+  new THREE.MeshStandardMaterial({ color: 0xff00ff }), // pink
+];
+const spheres = sphereMaterials.map((material) => new THREE.Mesh(sphereGeometry, material));
+spheres.forEach((sphere, index) => {
+  sphere.visible = index < 2; // Only the first two spheres are initially visible
+  scene.add(sphere);
+});
+
+// Initial Positions
+const initialPositions = [
+  { x: -2, y: 2, z: 0 }, // red
+  { x: 2, y: -2, z: 0 }, // green
+  { x: 0, y: 0, z: 0 }, // blue
+  { x: 0, y: 0, z: 0 }, // yellow
+  { x: 0, y: 0, z: 0 }, // aqua
+  { x: 0, y: 0, z: 0 }, // pink
+];
+
+spheres.forEach((sphere, index) => {
+  sphere.position.set(initialPositions[index]?.x || 0, initialPositions[index]?.y || 0, 0);
+});
+
+// Animation Variables
+const movementDuration = 5; // seconds
+const frameRate = 60; // frames per second
+let animationTime = 0;
+let phase = "floating"; // "floating", "eclipse", "scatter"
+let scatterStartTime;
+
+// Easing Functions
+function easeOutCubic(x) {
+  return 1 - Math.pow(1 - x, 3);
+}
+
+function easeOutExpo(x) {
+  return x === 1 ? 1 : 1 - Math.pow(2, -10 * x);
+}
+
+// Ellipse Parameters
+const ellipseXRadius = 0.5; // Radius of ellipse along the x-axis
+const ellipseYRadius = 0.25; // Radius of ellipse along the y-axis
+const ellipseSpeed = 0.01; // Reduced speed for slower movement
+
+// Floating Phase Logic
+function floatingPhase(sphere, index) {
+  let time = frameCount * ellipseSpeed;
+
+  // Check if the sphere is the first one (red ball)
+  if (index === 0) {
+    // Rotate in the opposite direction for the red ball
+    sphere.position.x = initialPositions[index].x + ellipseXRadius * Math.cos(-time);
+    sphere.position.y = initialPositions[index].y + ellipseYRadius * Math.sin(-time);
+  } else {
+    // Original rotation direction for other spheres
+    sphere.position.x = initialPositions[index].x + ellipseXRadius * Math.cos(time);
+    sphere.position.y = initialPositions[index].y + ellipseYRadius * Math.sin(time);
+  }
+}
+
+// Eclipse Phase Logic
+function eclipsePhase(sphere, initialPosition) {
+  animationTime += 0.001; // Slower increment
+  if (animationTime > 1) animationTime = 1;
+
+  let progress = easeOutExpo(animationTime);
+  sphere.position.x = THREE.MathUtils.lerp(initialPosition.x, 0, progress);
+  sphere.position.y = THREE.MathUtils.lerp(initialPosition.y, 0, progress);
+}
+
+// Target positions for each sphere
+const targets = [
+  { x: -1, y: 2 }, // red
+  { x: 1, y: 2 }, // green
+  { x: 2, y: 0 }, // blue
+  { x: 1, y: -2 }, // yellow
+  { x: -1, y: -2 }, // aqua
+  { x: -2, y: 0 }, // pink
+];
+
+function moveToTarget(sphere, target, startTime, currentTime) {
+  const duration = movementDuration * 1000; // Convert to milliseconds
+  let timeElapsed = currentTime - startTime;
+  if (timeElapsed > duration) timeElapsed = duration;
+
+  let progress = easeOutExpo(timeElapsed / duration);
+  sphere.position.x = THREE.MathUtils.lerp(sphere.position.x, target.x, progress);
+  sphere.position.y = THREE.MathUtils.lerp(sphere.position.y, target.y, progress);
+}
+
+// Event Listener for Phase Transition
+renderer.domElement.addEventListener("click", onPhaseChange);
+
+let frameCount = 0; // Initialize frameCount
+
+function onMouseMove(event) {
+  // Calculate mouse position in normalized device coordinates
+  // (-1 to +1) for both components
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+}
+
+function addTextToSpheres(font) {
   const textHeight = 0.05; // Height of the text
   const textDistanceFromSphere = 0.4; // Distance above the sphere surface
-
-  // Define the labels array outside the loop
   const labels = ["About", "Contacts", "Works", "Prices", "Advertisement", "Products"];
 
   spheres.forEach((sphere, index) => {
@@ -70,115 +174,9 @@ loader.load("https://threejs.org/examples/fonts/helvetiker_regular.typeface.json
     // Attach the text mesh to the sphere
     sphere.add(textMesh);
   });
-});
-
-function onMouseMove(event) {
-  // Calculate mouse position in normalized device coordinates
-  // (-1 to +1) for both components
-  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 }
 
-window.addEventListener("mousemove", onMouseMove, false);
-
-// Sphere Creation
-const sphereGeometry = new THREE.SphereGeometry(0.5, 32, 32);
-const sphereMaterials = [
-  new THREE.MeshStandardMaterial({ color: 0xff0000 }), // red
-  new THREE.MeshStandardMaterial({ color: 0x00ff00 }), // green
-  new THREE.MeshStandardMaterial({ color: 0x0000ff }), // blue
-  new THREE.MeshStandardMaterial({ color: 0xffff00 }), // yellow
-  new THREE.MeshStandardMaterial({ color: 0x00ffff }), // aqua
-  new THREE.MeshStandardMaterial({ color: 0xff00ff }), // pink
-];
-
-const spheres = sphereMaterials.map((material) => new THREE.Mesh(sphereGeometry, material));
-spheres.forEach((sphere, index) => {
-  sphere.visible = index < 2; // Only the first two spheres are initially visible
-  scene.add(sphere);
-});
-
-// Initial Positions
-const initialPositions = [
-  { x: -2, y: 2, z: 0 }, // red
-  { x: 2, y: -2, z: 0 }, // green
-  { x: 0, y: 0, z: 0 }, // blue
-  { x: 0, y: 0, z: 0 }, // yellow
-  { x: 0, y: 0, z: 0 }, // aqua
-  { x: 0, y: 0, z: 0 }, // pink
-];
-
-spheres.forEach((sphere, index) => {
-  sphere.position.set(initialPositions[index]?.x || 0, initialPositions[index]?.y || 0, 0);
-});
-
-// Animation Variables
-const movementDuration = 5; // seconds
-const frameRate = 60; // frames per second
-let animationTime = 0;
-let phase = "floating"; // "floating", "eclipse", "scatter"
-let scatterStartTime;
-
-// Easing Function
-function easeOutCubic(x) {
-  return 1 - Math.pow(1 - x, 3);
-}
-
-function easeOutExpo(x) {
-  return x === 1 ? 1 : 1 - Math.pow(2, -10 * x);
-}
-
-// Ellipse Parameters
-const ellipseXRadius = 0.5; // Radius of ellipse along the x-axis
-const ellipseYRadius = 0.25; // Radius of ellipse along the y-axis
-const ellipseSpeed = 0.01; // Reduced speed for slower movement
-
-function floatingPhase(sphere, index) {
-  let time = frameCount * ellipseSpeed;
-
-  // Check if the sphere is the first one (red ball)
-  if (index === 0) {
-    // Rotate in the opposite direction for the red ball
-    sphere.position.x = initialPositions[index].x + ellipseXRadius * Math.cos(-time);
-    sphere.position.y = initialPositions[index].y + ellipseYRadius * Math.sin(-time);
-  } else {
-    // Original rotation direction for other spheres
-    sphere.position.x = initialPositions[index].x + ellipseXRadius * Math.cos(time);
-    sphere.position.y = initialPositions[index].y + ellipseYRadius * Math.sin(time);
-  }
-}
-
-function eclipsePhase(sphere, initialPosition) {
-  animationTime += 0.001; // Slower increment
-  if (animationTime > 1) animationTime = 1;
-
-  let progress = easeOutExpo(animationTime);
-  sphere.position.x = THREE.MathUtils.lerp(initialPosition.x, 0, progress);
-  sphere.position.y = THREE.MathUtils.lerp(initialPosition.y, 0, progress);
-}
-
-// Target positions for each sphere
-const targets = [
-  { x: -1, y: 2 }, // red
-  { x: 1, y: 2 }, // green
-  { x: 2, y: 0 }, // blue
-  { x: 1, y: -2 }, // yellow
-  { x: -1, y: -2 }, // aqua
-  { x: -2, y: 0 }, // pink
-];
-
-function moveToTarget(sphere, target, startTime, currentTime) {
-  const duration = movementDuration * 1000; // Convert to milliseconds
-  let timeElapsed = currentTime - startTime;
-  if (timeElapsed > duration) timeElapsed = duration;
-
-  let progress = easeOutExpo(timeElapsed / duration);
-  sphere.position.x = THREE.MathUtils.lerp(sphere.position.x, target.x, progress);
-  sphere.position.y = THREE.MathUtils.lerp(sphere.position.y, target.y, progress);
-}
-
-// Event Listener for Phase Transition
-renderer.domElement.addEventListener("click", () => {
+function onPhaseChange() {
   if (phase === "floating") {
     phase = "eclipse";
     animationTime = 0;
@@ -192,9 +190,7 @@ renderer.domElement.addEventListener("click", () => {
     scatterStartTime = Date.now();
     spheres.forEach((sphere) => (sphere.visible = true));
   }
-});
-
-let frameCount = 0; // Initialize frameCount
+}
 
 function animate() {
   requestAnimationFrame(animate);
